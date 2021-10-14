505ec7e0c912039c2c227ad16f8f86ab4a8241b1
bpark12 <boae840622@gmail.com>
2021-10-13 22:36:58 -0400

    assignment3

--- data-service.js
+++ data-service.js
@@ -66,3 +66,124 @@ module.exports.getAllDepartments = function(){
         resolve(departments);
     })
 }
+
+module.exports.addEmployee = function(employeeData){
+    return new Promise((resolve,reject)=>{
+        if(!employeeData){
+           reject("no results returned");
+        }
+        else {
+           
+            if (employeeData.isManager==undefined) {
+                    employeeData.isManager=false;
+                }
+            else {
+                    employeeData.isManager=true;
+                }
+            
+            employeeData.employeeNum=employees.length+1;
+            employees.push(employeeData);
+            resolve();
+        }
+    })
+}
+
+module.exports.getEmployeesByStatus = function(status){
+    let empTemp=[];
+    return new Promise((resolve,reject)=>{
+        if (employees.length == 0) {
+            reject("no results returned");
+        }
+        else
+        {
+            for(let i=0; i<employees.length; i++)
+            {
+                if(employees[i].status==status)
+                {
+                    empTemp.push(employees[i]);
+                }
+            }
+            if(empTemp.length == 0)
+            {
+                    reject("no results returned"); 
+            }
+            else
+            resolve(empTemp);
+        }
+    })
+}
+
+module.exports.getEmployeesByDepartment = function(department){
+    let empTemp=[];
+    return new Promise((resolve,reject)=>{
+        if (employees.length == 0) {
+            reject("no results returned");
+        }
+        else
+        {
+            for(let i=0; i<employees.length; i++)
+            {
+                if(employees[i].department==department)
+                {
+                    empTemp.push(employees[i]);
+                }
+            }
+            if(empTemp.length == 0)
+            {
+                    reject("no results returned"); 
+            }
+            else
+            resolve(empTemp);
+        }
+    })
+}
+
+module.exports.getEmployeesByManager = function(manager){
+    let empTemp=[];
+    return new Promise((resolve,reject)=>{
+        if (employees.length == 0) {
+            reject("no results returned");
+        }
+        else
+        {
+            for(let i=0; i<employees.length; i++)
+            {
+                if(employees[i].employeeManagerNum==manager)
+                {
+                    empTemp.push(employees[i]);
+                }
+            }
+            if(empTemp.length == 0)
+            {
+                    reject("no results returned"); 
+            }
+            else
+            resolve(empTemp);
+        }
+    })
+}
+
+module.exports.getEmployeeByNum = function(num){
+    let empTemp;
+    return new Promise((resolve,reject)=>{
+        if (employees.length == 0) {
+            reject("no results returned");
+        }
+        else
+        {
+            for(let i=0; i<employees.length; i++)
+            {
+                if(employees[i].employeeNum==num)
+                {
+                    empTemp=employees[i];
+                }
+            }
+            if(!empTemp)
+            {
+                    reject("no results returned"); 
+            }
+            else
+            resolve(empTemp);
+        }
+    })
+}
--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 * of this assignment has been copied manually or electronically from any other source
 * (including 3rd party web sites) or distributed to other students.
 *
-* Name: Boae Park      Student ID: 075194100    Date: Sep 23, 2021
+* Name: Boae Park      Student ID: 075194100    Date: oct 10, 2021
 *
 * Online (Heroku) Link: https://desolate-chamber-07691.herokuapp.com/
 *
@@ -12,6 +12,8 @@
 
 var express=require("express");
 var app=express();
+const fs = require("fs");
+const multer = require("multer");
 var path=require("path");
 
 const dataService = require('./data-service.js');
@@ -22,18 +24,68 @@ function onHttpStart(){
     console.log("Express http server listening on: " + HTTP_PORT);
 }
 
-app.use(express.static('public'));
+const storage = multer.diskStorage({
+    destination: "./public/images/uploaded",
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + path.extname(file.originalname));
+    }
+  });
 
-app.get("/", function(req,res){
+const upload = multer({ storage: storage });
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("./public"));
+
+app.get("/", (req,res)=>{
     res.sendFile(path.join(__dirname,"/views/home.html"));
 });
 
-app.get("/about", function(req,res){
+app.get("/about", (req,res)=>{
     res.sendFile(path.join(__dirname,"/views/about.html"));
 });
 
+app.get("/employees/add", (req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/addEmployee.html"));
+});
+
+app.post("/employees/add",  (req, res) => {
+ 
+    dataService.addEmployee(req.body).then(()=>{
+        res.redirect("/employees");
+      }).catch((err)=>{
+        res.send(err);
+    });
+});
+
+
+app.get("/images/add", (req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/addImage.html"));
+});
+
+app.post("/images/add", upload.single("imageFile"), (req, res) => {
+    res.redirect("/images");
+});
+
+app.get("/images", (req,res)=>{
+    fs.readdir("./public/images/uploaded", function(err, items) {
+        res.json({"images": items}); 
+    });
+});
+
 app.get("/employees", (req,res) => {
+    if (req.query.status) {dataService.getEmployeesByStatus(req.query.status).then((data)=>{ res.json(data);}) }
+    if (req.query.department) { dataService.getEmployeesByDepartment(req.query.department).then((data)=>{ res.json(data);})}
+    if (req.query.manager) { dataService.getEmployeesByManager(req.query.manager).then((data)=>{ res.json(data);})}
     dataService.getAllMembers().then((data)=>{
+        res.json(data); 
+    }).catch((err)=>{
+        res.send(err);
+    });
+});
+
+app.get("/employee/:value", (req,res) => {
+    dataService.getEmployeeByNum(req.params.value).then((data)=>{
         res.json(data); 
     }).catch((err)=>{
         res.send(err);
